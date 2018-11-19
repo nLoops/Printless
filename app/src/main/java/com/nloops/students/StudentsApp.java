68458ad0a39f08dc4d0ab42f4456028c54b6ed5d
Ahmed Ibrahim <a.elgammal112@gmail.com>
2018-11-19 15:26:15 +0300

    Add (+) Init leak canary

--- app/src/main/java/com/nloops/students/StudentsApp.java
+++ app/src/main/java/com/nloops/students/StudentsApp.java
@@ -2,6 +2,7 @@ package com.nloops.students;
 
 import android.app.Application;
 import android.os.StrictMode;
+import com.squareup.leakcanary.LeakCanary;
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
@@ -10,6 +11,14 @@ public class StudentsApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
+    // Normal app init code...
 
     if (BuildConfig.DEBUG) {
       Timber.plant(new DebugTree());
