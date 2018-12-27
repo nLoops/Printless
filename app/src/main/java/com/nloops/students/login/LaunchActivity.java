e3eee54f0d2919516f79fa332ce9fbb9d9cfe4dd
Ahmed Ibrahim <a.elgammal112@gmail.com>
2018-12-28 00:43:58 +0300

    (-) Fix FirebaseAuth Listener exception.

--- app/src/main/java/com/nloops/students/login/LaunchActivity.java
+++ app/src/main/java/com/nloops/students/login/LaunchActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.nloops.students.R;
 import com.nloops.students.subjects.SubjectActivity;
 
@@ -19,51 +18,26 @@ public class LaunchActivity extends AppCompatActivity {
   @BindView(R.id.tablayout)
   TabLayout mTablayout;
 
-  private FirebaseAuth mAuth;
-  private FirebaseAuth.AuthStateListener mAuthListener;
-  private FirebaseUser user;
-
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_launch);
     // bind views
     ButterKnife.bind(this);
-    initFirebase();
+    checkUserState();
 
   }
 
-
-  private void initFirebase() {
-    //Define Firebase Auth.
-    mAuth = FirebaseAuth.getInstance();
-    // set the listener to Auth User.
-    mAuthListener = firebaseAuth -> {
-      user = firebaseAuth.getCurrentUser();
-      // if current user != null means (exist or signing in)
-      if (user != null) {
-        // User is signed in
-        // start subject home activity if we have registered account singed
-        startActivity(new Intent(this, SubjectActivity.class));
-        finish();
-      } // pager adapter
+  private void checkUserState() {
+    if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+      // User is signed in
+      // start subject home activity if we have registered account singed
+      startActivity(new Intent(this, SubjectActivity.class));
+      finish();
+    } else {
       LaunchFragmentAdapter adapter = new LaunchFragmentAdapter(getSupportFragmentManager());
       mPager.setAdapter(adapter);
       mTablayout.setupWithViewPager(mPager);
-    };
-
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    mAuth.addAuthStateListener(mAuthListener);
-  }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    mAuth.removeAuthStateListener(mAuthListener);
+    }
   }
 }
